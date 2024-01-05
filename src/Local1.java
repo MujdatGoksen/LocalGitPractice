ead61ab2ddd6a1477962fb2d7d286e8dcdfe1ea2
Mujdat Goksen <mujdatgoksen@gmail.com>
2024-01-05 20:42:09 +0100

    local bir eklendi

--- src/Local1.java
+++ src/Local1.java
@@ -1,5 +1,6 @@
 public class Local1 {
     public static void main(String[] args) {
+        System.out.println("burada degisiklik yaptim");
         System.out.println("Git Test");
         System.out.println("Yeni satir eklendi");
         System.out.println("Yeni Test");
--- src/mujdat.java
+++ src/mujdat.java
@@ -1,5 +1,6 @@
 public class mujdat {
     public static void main(String[] args) {
+        System.out.println("burada degisiklik yaptim");
 
         System.out.println("mujdat branc in da eklenen kod");
         System.out.println("yenilik");
